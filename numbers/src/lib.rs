0d75fa7f1f77a9b2557c14a6cb065f262c4ce129
Denys <denys@pop-os.localdomain>
2022-09-30 17:57:52 +0200

    changed the print fn in library from array to vectors

--- numbers/src/lib.rs
+++ numbers/src/lib.rs
@@ -2,9 +2,16 @@ pub fn say_hello() {
     println!("Hello, world!");
 }
 
+//pub fn print() {
+//    let numbers = [1,2,3,4,5];
+//    for number in numbers.iter() {
+//        println!("{}",number);
+//    }
+//}
+
 pub fn print() {
-    let numbers = [1,2,3,4,5];
-    for number in numbers.iter() {
+    let numbers = vec![1,2,3,4,5];
+    for number in numbers {
         println!("{}",number);
     }
 }
